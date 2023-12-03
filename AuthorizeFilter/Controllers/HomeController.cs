712cd09e30430aaa8b7ad3c7c8edd6df792a84d1
BALA MURALI KRISHNA PENUMARTHI <Balu5a3@users.noreply.github.com>
2023-12-02 23:29:16 -0500

    Initial commit

---
+++ AuthorizeFilter/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuthorizeFilter.Controllers
+{
+    public class HomeController : Controller
+    {
+        //[Route("Balu")]
+        public ActionResult Index()
+        {
+            //return Redirect("Home/About");
+
+            //return RedirectToAction("About");
+
+            //return RedirectToAction("Index","Admin");
+
+            return RedirectToRoute("Default");
+            
+        }
+
+
+        //[Authorize]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ AuthorizeFilter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AuthorizeFilter.Startup))]
+namespace AuthorizeFilter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
